2bcbc2d3255baa9f4deb8901de6db744bf109199
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-12-03 09:36:39 -0300

    Criação do HTML

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from flask import render_template, request, redirect, url_for, session, Flask
-import requests
+from flask import render_template, Flask
+
 
 df = pd.read_csv('LOPAL-ProjetoIntegrador-Esp8266_Receiver.csv')
 
@@ -8,15 +8,18 @@ print(df.to_numpy())
 for i in df.to_numpy():
     print(i)
 
-tabela = df.to_numpy()
+tabela = df.to_numpy()[::-1]
 
 app = Flask(__name__)
+with app.app_context():
+    def gerar_html_estatico():
+        html_gerado = render_template('template.html', tabela=tabela)
 
-@app.route('/')
-def index():
-    return render_template('index.html', tabela = tabela)
+        novo_arquivo = 'index.html'
+        with open(novo_arquivo, 'w', encoding='utf-8') as f:
+            f.write(html_gerado)
+        
+        print("HTML gerado com sucesso. Acesse o index.html")
 
-if __name__ == "__main__":
-    app.run()
-
-# url = 
+    if __name__ == "__main__":
+        gerar_html_estatico()
